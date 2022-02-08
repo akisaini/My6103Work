d62e1aa708a6f890148a4fdabaff428a5b98b316
Akshat Saini <akshatsaini1@gmail.com>
2022-02-08 14:19:27 -0500

    Added content in file.

--- mod2/Week02_basic/HW_basic.py
+++ mod2/Week02_basic/HW_basic.py
@@ -1,12 +1,33 @@
 #%%
 # print("Hello world!")
-
+print("Hello World!")
 
 #%%
 # Question 1: Create a Markdown cell with the followings:
 # Two paragraphs about yourself. In one of the paragraphs, give a hyperlink of a website 
 # that you want us to see. Can be about yourself, or something you like.
+#%%[markdown]
 
+# #  **Introduction about myself!**  :smile: 
+
+#I am Akshat, a first semester data science student. I have done my bachelors in computer science and 
+
+#worked as a software engineer for close to 4 years following that. During work I realized I would like to 
+
+#narrow my field of study in computers specific to analysis of data and machine learning. I have always been
+
+#very passionate about the concept of intelligent machines and natural language processing. 
+
+#
+
+#I personally had a chance to develop a very small natural language processing application with a team
+
+#during bachelors. It gave me an understanding of how data sets were to be created for simple pronouns, 
+
+#nouns, things and places. We created our own small and basic data set of different words that the 
+
+#application was trained on. [My Linkedin.](https://www.linkedin.com/in/akshat-saini/)
+ 
 
 #%%
 # Question 2: Create
@@ -14,11 +35,24 @@
 # Have at least 6 classes, even if you are not a DS major
 # Then print out the last entry in your list.
 
+classes = ['Data Mining', 'Data Warehousing', 'Data Science', 'Machine Learning 1', 'Algorithm Design for Data Science', 'Natural Language Processing']
+
+for element in classes:
+    print(element)
+    
 
 #%%
 # Question 3: After you completed question 2, you feel Intro to data mining is too stupid, so you are going 
 # to replace it with Intro to Coal mining. Do that in python here.
 
+classes = ['Data Mining', 'Data Warehousing', 'Data Science', 'Machine Learning 1', 'Algorithm Design for Data Science', 'Natural Language Processing']
+
+for i in range(len(classes)):
+    if classes[i] == 'Data Mining':
+        classes[i] = 'Coal Mining'
+    else:
+        pass
+print(classes)      
 
 #%%
 # Question 4: Before you go see your acadmic advisor, you are 
@@ -27,10 +61,19 @@
 # probably doesn't like coal. And that coal mining class doesn't even have a 
 # course number.
 
+classdictionary = {6103:'Data Mining', 6102:'Data Warehousing', 6101:'Data Science', 6202:'Machine Learning 1', 6001:'Algorithm Design for Data Science', 6312:'Natural Language Processing'}
+
+for k, v in classdictionary.items():
+    print(f'{k}:{v}')
+
 #%%
 # Question 5: print out and show your advisor how many 
 # classes (print out the number, not the list/dictionary) you plan 
 # to take.
+
+classdictionary = {6103:'Data Mining', 6102:'Data Warehousing', 6101:'Data Science', 6202:'Machine Learning 1', 6001:'Algorithm Design for Data Science', 6312:'Natural Language Processing'}
+
+print(f'I plan on taking {len(classdictionary)} classes.')
 
 #%%
 # Question 6: Using loops 
@@ -53,6 +96,10 @@
 # ...
 # You might find something like this useful, especially if you use the remainder property x%7
 dayofweektuple = ('Sun','Mon','Tue','Wed','Thu','Fri','Sat') # day-of-week-tuple
+n=31
+for i in range(1,n+1):
+    print(f'{dayofweektuple[(i-2)%7]} - 2022/1/{i}')
+         
 
 
 # %%[markdown]
@@ -64,9 +111,27 @@ dayofweektuple = ('Sun','Mon','Tue','Wed','Thu','Fri','Sat') # day-of-week-tuple
 # Write python codes that converts seconds, say 257364 seconds,  to 
 # (x Hour, x min, x seconds)
 # ----------------------------------------------------------------
+import math
 
+def time_conv(n): # 7500 - 2 hr 5 min 0 sec
+    hours = (n / 3600) #2.0833..
+    if hours%2 == 0: 
+        return print(f'{hours} : hours')
+    else:
+        t1 = math.floor(hours) #2.0
+    mins = (hours - t1) * 60 #2.0833 - 2 * 60 = 5.000009
+    if mins%2==0:
+        return print(f'{mins} : mins')
+    else:
+        t2 = (math.floor(mins)) #5
+    sec = (mins - t2) * 60   #5.000009-5
+    if sec%2==0:
+        return print(f'{sec} : sec')
+    else:
+        t3 = math.floor(sec)
+    return print(f'{t1} :Hour, {t2} :Min, {t3} :Sec')
 
-
+time_conv(7999)
 
 
 #%%
@@ -77,8 +142,24 @@ dayofweektuple = ('Sun','Mon','Tue','Wed','Thu','Fri','Sat') # day-of-week-tuple
 # Hint: one way is to create three nested loops.
 # ----------------------------------------------------------------
 
-
-
+def perms(val):
+    if len(val) == 0: #no value given, return empty list
+        return []
+    elif len(val) == 1: #value given is a single letter 
+        return [val]
+    # else 2 or more letters are given as input
+    else:
+        lst = []
+        
+        for i in range(0, len(val)): #length = 3
+            p1 = val[i] #first element
+            p2 = val[:i]+val[i+1:] #all ele till but not including the first ele + all ele after the first ele.
+            for j in perms(p2):
+                lst.append(p1+j)
+        
+        return lst        
+        
+perms('abc')  
 
 
 #%%
@@ -103,6 +184,14 @@ dayofweektuple = ('Sun','Mon','Tue','Wed','Thu','Fri','Sat') # day-of-week-tuple
 #   *******
 #  *********
 # ----------------------------------------------------------------
+def chrtree():
+    level = int(input('Enter the height of the tree!'))
+    star = 1
+    
+    for i in range(level):
+        print(" " * (level-i) + '*' * star )
+        star+=2
 
+chrtree()
 
-
+# %%
--- mod2/Week02_basic/prac.py
+++ mod2/Week02_basic/prac.py
@@ -1,15 +1,32 @@
+#%%
 #Python code to remove empty list from an existing list. 
 # [5,4,3,[],7,[]] -> [5,4,3,7]
 
-
 def cleanlst():
-    lt = input('Provide elements of list separated by ','')
+    # lt = input('Provide elements of list separated by')
+    lt = "5,6,8,[]"
     lt_clean = list(lt.split(','))
     
     for i in range(len(lt_clean)):
         if lt_clean[i] == '[]':
-            lt_clean[i].pop()
+            lt_clean.pop()
 
     return lt_clean
 
 cleanlst()
+#%%
+import math                                                                          
+def convgrade():
+    grade = float(input('Enter your grade in numbers!'))
+    if grade >= 93:
+        final = 'A'
+    elif 90<=grade<93:
+        final = ['A-']
+    elif 87<=grade<90:
+        final = ['B+']
+    elif 80<grade<87:
+        final = ['B']
+    
+    return final
+
+#%%
