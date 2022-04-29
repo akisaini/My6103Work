41f020e7fa0b8835bd4247dbb0815867dd69994f
Akshat Saini <akshatsaini1@gmail.com>
2022-04-29 19:50:07 -0400

    Update to midterm_saini_Akshat.py.

--- midterm/6103_10_Midterm_Saini_Akshat.py
+++ midterm/6103_10_Midterm_Saini_Akshat.py
@@ -50,58 +50,57 @@ print("\nReady to continue.")
 #
 
 #%% [markdown]
+#----------------------------------------------------------------------
 # *What is a utopian society?*
-# The concept of utopia refers to a perfect or ideal civilization. It typically describes an imaginary community or society that possesses highly desirable or nearly perfect qualities for its members.
-# Utopias focus on - amongst other things - equality, in such categories as economics, government and justice, with the method and structure of proposed implementation varying based on ideology. 
+#
+# The concept of utopia refers to a perfect or ideal civilization. It typically describes an imaginary community or society that possesses highly desirable or nearly perfect qualities for its members. 
+#
 # There is no consensus as to exactly what a utopia should look like, but values like freedom, security, equality, and enlightened thinking are all common features in utopian discussions and literature.
-
+#
 # *Utopian Society Ideals:*
 # Theoretical utopian societies usually provide their citizens with equal rights, a life without fear, economic security, and collective or government-provided welfare for all.
-
+#
 # Some common utopia examples to relate with include scenarios like:  
 # * The Federation in the Star Trek series. 
 # * The Capital in The Hunger Games series, a place of luxury and freedom.
 # * England in Aldous Huxley's Brave New World, a place with no wars or hunger but also no emotion.
-
-# In the 2021 Best Countires in the World Ranking, the overall ranking of best countries measured global performace on a variety of metrics, amongst which 'literacy' was one of the top criterias, followed by 'equality/racial equality' and an 'economically stable job market'.
-
+#
+# In the '2021 Best Countires in the World' Ranking conducted by USNews, the overall ranking of best countries measured global performace on a variety of metrics, amongst which 'literacy' was one of the top criterias, followed by 'equality/racial equality' and an 'economically stable job market'.
+#
 # In our model, we will try to focus more on the above three aspects to compare the 2 different worlds to the definition of Utopia. 
-
-# *Basic EDA:*
+#
+#
+#%%
+# Basic EDA:
+#
 # Using the info function to find the general datatypes and information of all the variables in world1 and world2 data sets:
+#world1.info()
+#world2.info()
 #
-#%%
-world1.info()
-world2.info()
-#
-#%%
 # using the describe method to compare 'mean' values in differenr variables:
+#world1.describe()
+#world2.describe()
 #
-world1.describe()
-world2.describe()
 # The mean age in both the worlds is 44 years for the population. 
 # Years of education is 15 years in both worlds as well. 
 #
 # We could use pivot table here to capture gender mean values for certain variables like age00 or income00 etc. , but grouping should work better. 
 gender_pivot = pd.pivot_table(data = world1, index = 'gender', values = 'age00', aggfunc= 'mean')
-print(gender_pivot)
+#print(gender_pivot)
 # grouping gender and comparing the variables in different worlds using aggregate function 'sum' and 'mean':
 grouped1 = world1.groupby('gender').agg([np.sum, np.mean])
-print(grouped1)
+#print(grouped1)
 #
 # In world1 we can see that average age for both men and women is 
 # 44 years and both the genders are educated an average of 15 years. 
 # It can be noted however that men earn an average of 11000 more than women annually in world1. *So there is gender pay gap.* 
 #
-#
 # Now grouping gender and comparing the variables in world2:
 grouped2 = world2.groupby('gender').agg([np.sum, np.mean])
-print(grouped2)
+#print(grouped2)
 #
 # In wordl2 we can note that the average age of both men and women is 44 years and both the genders go through an overall education of 15 years(avg) as calculated. 
 # There is relatively no pay gap between the two genders compared to world1. And both men and women tend to earn an average amount of 60,000 anually. 
-#
-#
 #
 # Creating subsets based on gender and then comparing different variables between the two worlds. 
 #
@@ -134,7 +133,6 @@ w2_ethnic_0 = ethnic_grouped2.get_group(0)
 w2_ethnic_1 = ethnic_grouped2.get_group(1)
 w2_ethnic_2 = ethnic_grouped2.get_group(2)
 
-#%%
 #define data
 data = [w1_ethnic_0.shape[0], w1_ethnic_1.shape[0], w1_ethnic_2.shape[0]]
 labels = ['ethnic_0', 'ethnic_1', 'ethnic_2']
@@ -390,13 +388,10 @@ plt.subplots_adjust(top=.80, wspace = 1.00)
 plt.show()
 
 # The plots show similar distribution between industry employment when comparing the two worlds. Retail(1), health(3) and professional n business(6) tend to be the biggest employers in both the worlds. 
-
-# --------------------------------------------------------------
-
+#
 # Aside from this, in world1 it can be noted that 'Construction'(4) is very uncommon among women and mostly a male dominated sector. 
 # Retail(1), Education(2) and Health(3) are very popular among women, being one of the bigger employers, as compared to men. 
 # Professional n business(6), Construction(4) and finance(7) tend to be dominated by males in world 1. 
-
 
 x = np.arange(0, 8, 1)
 plt.subplot(1,2,1)
@@ -470,20 +465,37 @@ sns.catplot(x = 'ethnic', y = 'income00', hue = 'gender', kind = "boxen", col = 
 
 plt.show()
 
-#Summary and Conclusion: 
-
-# Population and other variables like marital status, ethinicity, age and education are very identical between the two worlds. 
+#%% [markdown]
+#----------------------------------------------------------------------
+# # Summary and Conclusion: 
 # 
-# Below findings stand out: 
+# In conclusion of all the analysis and findings from the plots and graphs we can clearly observe that World 2 is more equally divided or more equally set up when it comes to socio-economic factors like income, industry preference, education years within the different ethnicities, genders etc. 
+# 
+# World 2 tends to lean more towards our definition of Utopia or a fair world, when compared to World 1. To make a choice, I would consider World 2 utopian.  
 #
-# Gender income gap exists in world 1. Mean annual income00 for men tends to be more than women. 
-# Industry division/gap between genders can be noticed in world1. 
-# Avg income is nearly same for men and women in world 2. 
-# There exists a mean annual Income gap of 11k in world 1. 
-# When not considering gender, average annual income is identical between the two worlds.(60k) 
-# More infomation might have been useful such as crime stati
-# stics, weather, and happiness quotient/life expectency etc. But solely based on the given information, World 2 is probably close to what we are seeking. But World 1 is still very livable. 
-# We create our own utopia with family and friends and that can be created anywhere, even in a jungle. 
-# %%
-
-# Different ethnicities are equally divided in terms of population in both the worlds! No one ethnicity is dominant in either world. 
+# 
+# *Below findings stand out*:
+#
+#  0. Overall population and other variables like marital status, ethinicity, age and education are very identical between the two worlds. 
+#  1. Different ethnicities are equally divided in terms of population in both the worlds! No one ethnicity is dominant in either world. 
+#  2. Average Age and Education between ethnicities is same for all three ethnicities in both the worlds!
+#  3. In World 1, It can be seen that ethnicity 0 has more people working in Health, Retail and Education sectors than manufacturing or Finance.
+# Ethnicity 1 has more people working in Hospitality, Health, Retail and Construction sectors. And very little in Manufacturing or Finance. 
+# Ethnicity 2 has majority people working in Professional n Business, Manufacturing and Finance, and just 1% people working in the Construction sector.  
+#  4. Overall, ethnicities in World 2 are better divided within the different sectors as compared to world 1. 
+# Ethnicity 0 has equal preference in Retail, Professional n Business, and Health. And prefers Finance the least (8%). But preference is well divided over the sectors. 
+# Ethnicity 1 has higher preference for Retail, Professional n Business, Education, Manufacturing, Health over Construction or Finance (9%). But still well divided. 
+# Ethnicity 2 has higher preference for Retail, Education, Health, Manufacturing, Professional n Business as compared to construction or finance (8%). But still well divided. 
+#  5. Moreover, we can observe that World 2 is equally divided when it comes to earnings and industry preference within the genders in the different ethinicities as well! We can also make the observation that all three ethnicities have similar quartile ranges in the violin plots for both income and industry. 
+# In world 1, however, things are more divided. From the violin plots, we can clearly observe that Women of ethnicity 2 are more involved in industry sectors 5,6,and 7 as compared to women in ethnicity 1 and 0. Same can be said for men. 
+# When it comes to earnings, men of ethnicity 2 earn more than men of ethnicity 0 and 1. Also, Their income is spread over a wider range. 
+#  6. Within the genders in World 1, it can be noted that 'Construction', 'Finance' and 'Professional n business' is very uncommon among women and mostly a male dominated sector. Retail, Education and Health are very popular among women, being one of the bigger employers, as compared to men. 
+#  7. Employment between men and women is more equally distributed as compared to world 1. Even in businesses like contruction, women are equally employed in world 2, with Retail, Education and Health sectors  being very good souces of employment for men in world 2 contrary to world 1. 
+#  8. Overall marital status compared between the two worlds is extremely identical. In world 1 there are some men who are still unmarried or divorced compared to world 2.
+#  9. From the categorical Boxen plots for World 2, we can clearly observe that the education years, industry preference and mean income is similar for all marital groups belonging to different ethnicities. However, For World 1, different ethnicities have variations in those variables. 
+#
+#
+# As a result, *World 2* tends to lean more towards our definition of a Utopian or a fair society, which is 'Literacy', 'Racial Equality' and 'Economically Stable Job Market'.
+#
+#----------------------------------------------------------------------
+#%%
