a5e69da8d27adc1a76bbc8454deb183956e02d44
Akshat Saini <akshatsaini1@gmail.com>
2022-05-04 20:22:30 -0400

    Update dl.py.

--- dl.py
+++ dl.py
@@ -164,4 +164,138 @@ plt.xlabel('epochs')
 plt.ylabel('cost')
 plt.plot(epochlist, costlist)
 # %%
+#------------------------------------------------------------------
+# Telco Customer Churn ANN  - Artificial Neural Network. 
 
+telco = pd.read_csv('TelcoCustomerChurn.csv')
+telco = telco.drop('customerID', axis = 1)
+# %%
+# Converting string values to numeric values. 
+telco['TotalCharges'] = pd.to_numeric(telco['TotalCharges'], errors = 'coerce')
+# %%
+df2 = telco.dropna()
+
+# %%
+# Prints unique values in each column:
+def print_unique_vals(df):
+    for columns in df:
+     print(f'{columns}: {df[columns].unique()}')
+# %%
+
+df2.replace('No internet service', 'No', inplace = True)
+df2.replace('No phone service' 'No', inplace = True)
+df2['gender'].replace({'Female':1, 'Male':0}, inplace = True)
+#['Partner', 'Dependents', 'PhoneService', ]
+# %%
+cols_to_rep = ['Partner', 'Dependents', 'PhoneService', 'OnlineSecurity', 'MultipleLines', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'PaperlessBilling', 'Churn']
+
+for i in cols_to_rep:
+    df2[i].replace({'No':0, 'Yes':1}, inplace = True)
+# %%
+dummies_cols = ['InternetService', 'Contract', 'PaymentMethod']
+df2 = pd.get_dummies(data = df2, columns = dummies_cols)
+# %%
+cols_to_scale = ['tenure', 'MonthlyCharges', 'TotalCharges']
+scale = MinMaxScaler()
+for i in cols_to_scale:
+    df2[cols_to_scale] = scale.fit_transform(df2[cols_to_scale])
+# %%
+# Train/Test Split:
+X = df2.drop('Churn', axis = 1)
+y = df2['Churn']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=30)
+# %%
+# Now, building model using Tensorflow/Keras: (ANN)
+import tensorflow as tf
+from tensorflow import keras
+# %%
+model = keras.Sequential([
+    keras.layers.Dense(26, input_shape = (26,), activation = 'relu'),
+    keras.layers.Dense(10, activation = 'relu'),
+    keras.layers.Dense(1, activation = 'sigmoid')
+])
+
+model.compile(optimizer = 'adam',
+              loss = 'binary_crossentropy',
+              metrics = ['accuracy'])
+
+model.fit(X_train, y_train, epochs = 100)
+# %%
+y_pred = model.predict(X_test)
+y_pr = []
+for element in y_pred:
+    if element > 0.5:
+        y_pr.append(1)
+    else:
+        y_pr.append(0)
+
+# %%
+y_pr[:10]
+#%%
+y_test[:10]
+# %%
+from sklearn.metrics import confusion_matrix, classification_report
+
+cm = confusion_matrix(y_test, y_pr)
+
+import seaborn as sn
+sn.heatmap(cm, annot = True, fmt = 'd')
+
+cr = classification_report(y_test, y_pr)
+print(cr)
+# %%
+#------------------------------------------------------------------
+# Small image classification using CNN or convolutional neural networks.
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# %%
+(X_train, y_train), (X_test, y_test) = datasets.cifar10.load_data()
+# %%
+# 32x32x3 image. 32pixels by 32pixels by 3rgb = 3072 pixels in one image. 
+plt.imshow(X_train[1])
+# %%
+# contains values from 0 to 9. 10 categories in total. 
+y_train = y_train.reshape(-1,)
+y_test = y_test.reshape(-1,)
+# %%
+from sklearn.preprocessing import MinMaxScaler
+imagescale  = MinMaxScaler()
+# Cannot use minmaxscaler as dataset is 4d array. MinMaxScaler expects <=2D.  
+# Will scale by dividing by 255 as image is 256 bit (grayscale). 
+X_train = X_train/255
+X_test = X_test/255
+# %%
+# Now, building CNN model: 
+# kernel_size is the filter matrix[] size
+cnnmodel = keras.Sequential([
+    # we are telling it to have 32 filters. filter detection pattern to be a 3x3 matrix (kernel)
+    layers.Conv2D(filters = 32, kernel_size = (3,3), activation = 'relu', input_shape = (32,32,3)),
+    layers.MaxPooling2D((2,2)),
+    
+    layers.Conv2D(filters = 64, kernel_size = (3,3), activation = 'relu'),
+    layers.MaxPooling2D((2,2)),
+    
+    layers.Flatten(),
+    layers.Dense(64, activation = 'relu'),
+    layers.Dense(10, activation = 'softmax')
+    
+])
+
+cnnmodel.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics=  ['accuracy'])
+
+cnnmodel.fit(X_train, y_train, epochs = 10)
+# %%
+cnnpred = cnnmodel.predict(X_test)
+# %%
+# cnnpred is a distribution between 0 and 1 due to choosing softmax. The max value is the correct prediction. 
+
+for i in cnnpred:
+    print(np.argmax(i))
+        
+# %%
