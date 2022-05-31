2e1367001bb470938bf56533ff9cd2a3009feeb4
Akshat Saini <akshatsaini1@gmail.com>
2022-05-31 19:40:10 -0400

    Set up fastapi, uvicorn, and basic set up.

--- tomato_cnn/main.py
+++ tomato_cnn/main.py
@@ -1,4 +1,26 @@
 # Setting up FASTapi server
 #%%
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+# Below is very important for uvicorn to work.
+import nest_asyncio
+nest_asyncio.apply()
+#%%
+app = FastAPI()
+#%%
+@app.get("/ping")
+async def ping():
+    return {"Hello": "I am on"}
+
+@app.post('/predict')
+async def predict(
+    file: UploadFile = File(...)
+):
+    
+    pass
+
+#%%
+if __name__ == '__main__':
+    uvicorn.run(app, host = 'localhost')
 # %%
+
