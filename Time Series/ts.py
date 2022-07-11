ccad176cf9b40f2247e43f52283499158f55da99
Akshat Saini <akshatsaini1@gmail.com>
2022-07-11 19:32:39 -0400

    Update ts.py

---
+++ Time Series/ts.py
@@ -0,0 +1,19 @@
+#%%
+import pandas as pd
+import pandas_datareader as pdr
+from datetime import datetime
+import matplotlib.pyplot as plt 
+#%%
+df = pdr.get_data_yahoo('TSLA')
+# %%
+dstart = datetime(2020, 1, 1)
+dend = datetime(2021, 9, 1)
+
+plt.figure(figsize = (12,4))
+plt.plot(df['High'])
+plt.xlim([dstart, dend]) # 2020 Jan 01 to 2021 Sept 01 records.
+plt.ylim([0,900])
+plt.xticks(rotation = 30)
+plt.show()
+#df['High'].plot(figsize = (12,4))
+# %%
