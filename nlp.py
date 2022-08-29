1bc1ae907e7c4ca65bf6498131d172106e0533dd
Akshat Saini <akshatsaini1@gmail.com>
2022-08-28 21:24:22 -0400

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -138,3 +138,114 @@ df['Spam'] = df['Category'].apply(lambda x : 1 if x =='spam' else 0)
 # x = message, y = result
 X_train, X_test, y_train, y_test = train_test_split(df['Message'], df['Spam'], test_size=0.25, random_state = 10)
 # %%
+X_train[:4]
+y_train[:4]
+# %%
+from sklearn.feature_extraction.text import CountVectorizer
+
+#creates a vocab and then generates a vector for the messages based on that vocab. 
+vectorizer = CountVectorizer()
+#X_train.values converts it to Numpy array. 
+#type(X_train) - pandas.core.series.Series
+X_train_cv = vectorizer.fit_transform(X_train.values)
+
+# %%
+X_train_cv.toarray().shape
+# %%
+vectorizer.get_feature_names_out()
+#dir(vectorizer) will show all the methods of the vectorizer object
+#vocabulary_ gives the index for the word in the vocab. 
+vectorizer.vocabulary_
+# %%
+# Will fetch word at the 4216 index - 'me'
+vectorizer.get_feature_names_out()[4216]
+# %%# %%
+X_train_np = X_train_cv.toarray()
+# %%
+#1st email is represented as this in numbers. All the words that are in the vocab are marked as 1. Rest are marked as 0. 
+X_train_np[0]
+# %%
+np.where(X_train_np[1] != 0)
+# %%
+vectorizer.get_feature_names_out()[4026]
+# %%
+from sklearn.naive_bayes import MultinomialNB
+
+model = MultinomialNB()
+
+# %%
+model.fit(X_train_cv, y_train)
+# %%
+X_test_cv = vectorizer.transform(X_test)
+
+# %%
+from sklearn.metrics import classification_report
+y_pred = model.predict(X_test_cv)
+# %%
+# STOP WORDS using SPACY
+
+import spacy
+
+from spacy.lang.en.stop_words import STOP_WORDS
+
+len(STOP_WORDS)
+# %%
+#'english_core_web_small' - nlp english package
+nlp = spacy.load('en_core_web_sm')
+
+# %%
+doc = nlp('hey whats up')
+for token in doc:
+    if token.is_stop:
+        print(token)
+# %%
+def preprocess(text):
+    doc = nlp(text)
+    
+#token.text returns a string of the token. Just using token will return type spacy.tokens.token.Token
+    no_stop_words = [token.text for token in doc if not token.is_stop and not token.is_punct]
+    return no_stop_words
+
+# %%
+def preprocess(text):
+    doc = nlp(text)
+    no_stop_words = [token.text for token in doc if not token.is_stop]
+    return " ".join(no_stop_words) 
+# %%
+preprocess('hey whats up')
+# %%
+nlp.vocab['not'].is_stop = False
+# %%
+positive_text = preprocess('this is a good movie')
+negative_text = preprocess('this is not a good movie')
+# %%
+def preprocess(text):
+    doc = nlp(text)
+    no_stop_words = [token.text for token in doc if not token.is_stop and not token.is_punct]
+    return no_stop_words
+    
+text = ''' The India men's national cricket team, also known as Team India or the Men in Blue, represents India in men's international cricket.
+It is governed by the Board of Control for Cricket in India (BCCI), and is a Full Member of the International Cricket Council (ICC) with Test,
+One Day International (ODI) and Twenty20 International (T20I) status. Cricket was introduced to India by British sailors in the 18th century, and the 
+first cricket club was established in 1792. India's national cricket team played its first Test match on 25 June 1932 at Lord's, becoming the sixth team to be
+granted test cricket status.
+'''
+#%%
+tok = preprocess(text)
+# %%
+len(tok)
+# %%
+#create a dict and iterate through token to count their value. 
+frequency = {}
+for i in tok:
+    if i != '\n' and i != ' ':
+       if i not in frequency:
+           frequency[i] = 1 # if a token occurs for first time we initialize it to 1
+       else:
+           frequency[i] += 1 # if token is being repeated, we increment it by 1.      
+            
+    
+# %%
+[k for k, v in frequency.items() if v==max(frequency.values())]
+
+# %%
